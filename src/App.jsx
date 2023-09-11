d7f8f3fd26eb3c3e0d9eb3c8112575ffb99cf5d9
Micaela Estigarribia <95941951+M-Estigarribia@users.noreply.github.com>
2023-09-10 21:16:41 -0300

    Updated Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
-
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Routes/Home";
+import Layout from "./Routes/Layout"
+import Detail from "./Routes/Detail"
+import Contact from "./Routes/Contact"
+import Favs from "./Routes/Favs"
+import { ContextProvider } from "./Components/utils/global.context";
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <ContextProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"element={<Layout/>}>
+          <Route path="home" element={<Home/>}/>
+          <Route path="dentist/:id" element={<Detail/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="favs" element={<Favs/>}/>
+        </Route>
+      </Routes>
+      
+    </BrowserRouter>
+    </ContextProvider>
   );
 }
 
