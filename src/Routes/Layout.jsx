7bff2b3370bd34799704c182b19d75772058eac8
Micaela Estigarribia <95941951+M-Estigarribia@users.noreply.github.com>
2023-09-10 21:16:25 -0300

    Updated Favs, Home and Layout pages

--- src/Routes/Layout.jsx
+++ src/Routes/Layout.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from "../Components/Footer";
+import Navbar from "../Components/Navbar";
 
 const Layout = () => {
     return (
         <div>
             <Navbar/>
+            <Outlet/>
             <Footer/>
         </div>
     )
 }
 
-export default Layout
+export default Layout;
