b8086bcbc73534e1b8833b60de45c9890592ebc4
Micaela Estigarribia <95941951+M-Estigarribia@users.noreply.github.com>
2023-09-15 18:14:08 -0300

    Added styles to navigation and theme button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,42 +1,61 @@
-import React, { useContext, useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext, useState, useEffect} from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { ContextGlobal } from "./utils/global.context";
+import '../Styles/navbar.css';
 
 const Navbar = () => {
   const {state, dispatch} = useContext(ContextGlobal);
   const [changeThemeIcon, setChangeThemeIcon] = useState("/images/moon-icon.svg");
   const [logoImage, setLogoImage] = useState("/images/logo.svg");
-  function handleThemeChange() {
+  const [activeTab, setActiveTab] = useState("");
+  const [isAnimating, setIsAnimating] = useState(false);
+  const location = useLocation();
 
+  useEffect(() =>{
+    setActiveTab(location.pathname);
+  }, [location]);
+
+  const handleThemeChange = () => {
     if(state.theme === "light" ){
       dispatch({ type: "themeChange", payload: "dark" });
       setChangeThemeIcon("/images/sun-icon.svg");
       setLogoImage("/images/logo-dark.svg");
-    } else {
+    } 
+    else {
       dispatch({ type: "themeChange", payload: "light" });
       setChangeThemeIcon("/images/moon-icon.svg");
       setLogoImage("/images/logo.svg");
     }
   };
 
+  const startAnimation = () => {
+    setIsAnimating(true);
+  }
+
+  const endAnimation = () => {
+    setIsAnimating(false);
+  }
+
   return (
     <nav>
-      <div>
+      <div className='navbar-logo-container'>
         <Link to={`/`}>
-          <img src={logoImage} alt="logo" className='logo'/>
+          <img src={logoImage} alt="logo" className='navbar-logo'/>
         </Link>
       </div>
-      <div>
-        <Link to={`/`}>Inicio</Link>
-        {/* <Link to={`/home`}>Home</Link> */}
-        <Link to={`/contact`}>Contacto</Link>
-        <Link to={`/favs`}>Favoritos</Link>
-        <button className="rounded-button" onClick={handleThemeChange}>
-          <img className='icon' src={changeThemeIcon} alt="" />
+      <div className='navbar-links-container'>
+        <div className='navbar-links'>
+          <Link to={`/`} className={activeTab === '/' ? 'active': ''}>Inicio</Link>
+          <Link to={`/contact`} className={activeTab === '/contact' ? 'active' : ''}>Contacto</Link>
+          <Link to={`/favs`} className={activeTab === '/favs' ? 'active' : ''}>Favoritos</Link>
+          <div className="navbar-underline" style={{ left: activeTab === '/' ? '0%' : activeTab === '/contact' ? '33%' : '66%' }}></div>
+        </div>
+        <button className="rounded-button change-theme-button" onClick={handleThemeChange} onMouseEnter={startAnimation} onAnimationEnd={endAnimation}>
+          <img className={`icon ${isAnimating && 'animate-theme-button'}`} src={changeThemeIcon} alt="" />
         </button>
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
